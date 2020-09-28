1af5d3c4758f3cf912b54b9da39dcd9d50e097ce
Adam King <aking@twilio.com>
2020-09-28 10:27:47 +0100

    Create PaymentActions.js
    State refactoring as per CWeems

---
+++ src/states/PaymentActions.js
@@ -0,0 +1,57 @@
+const UPDATE_SYNC_PAYMENT_SID = "UPDATE_SYNC_PAYMENT_SID";
+const ADD_SMS_PAYMENT_EVENT = "ADD_SMS_PAYMENT_EVENT";
+const SHOW_VOICE_PAYMENT_FORM = "SHOW_VOICE_PAYMENT_FORM";
+const HIDE_VOICE_PAYMENT_FORM = "HIDE_VOICE_PAYMENT_FORM";
+
+const initialState = {
+    syncPaymentSid: {
+        token: null,
+        sid: null,
+        subscribeSync: false,
+    },
+    showVoicePaymentForm: false,
+};
+
+// Define plugin actions
+export class Actions {
+    static updateSyncPaymentSid = (payload) => ({
+        type: UPDATE_SYNC_PAYMENT_SID,
+        payload: payload,
+    });
+    static showVoicePaymentForm = () => ({
+        type: SHOW_VOICE_PAYMENT_FORM,
+    });
+
+    static hideVoicePaymentForm = () => ({
+        type: HIDE_VOICE_PAYMENT_FORM,
+    });
+}
+
+// Define how actions influence state
+export function reduce(state = initialState, action) {
+    switch (action.type) {
+        case UPDATE_SYNC_PAYMENT_SID: {
+            return {
+                ...state,
+                syncPaymentSid: action.payload,
+            };
+        }
+        
+        case SHOW_VOICE_PAYMENT_FORM: {
+            return {
+                ...state,
+                showVoicePaymentForm: true,
+            };
+        }
+
+        case HIDE_VOICE_PAYMENT_FORM: {
+            return {
+                ...state,
+                showVoicePaymentForm: false,
+            };
+        }
+
+        default:
+            return state;
+    }
+}
